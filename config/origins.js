e6b8cf8960705a1b09016bf907b185b1f8f0e6ee
K4T0 <talentounido.kano@gmail.com>
2023-04-15 23:02:33 -0600

    origins

--- config/origins.js
+++ config/origins.js
@@ -1,6 +1,7 @@
  const allowedOrigins = [
     'http://localhost:3000',
-    'http://localhost:8080'
+    'http://localhost:8080',
+    'https://witty-sky-016258010.3.azurestaticapps.net'
 ]
 
 export default allowedOrigins
