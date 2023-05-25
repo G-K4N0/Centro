8ef578ca54a86aef8f99f439679a3430297a9fad
K4T0 <talentounido.kano@gmail.com>
2023-05-25 09:46:11 -0600

    origenes de pruebas

--- config/origins.js
+++ config/origins.js
@@ -1,6 +1,8 @@
  const allowedOrigins = [
     'http://localhost:3000',
-    'http://localhost:8080'
+    'http://localhost:8080',
+   'http://192.168.3.117:3000',
+   'http://192.168.3.117:8080'
 ]
 
 export default allowedOrigins
