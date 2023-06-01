3d926a9b2e7c3f811e534e89cae70b8932dcaece
K4T0 <talentounido.kano@gmail.com>
2023-06-01 16:26:10 -0600

    variable de entorno para cors

--- config/origins.js
+++ config/origins.js
@@ -1,8 +1,16 @@
- const allowedOrigins = [
+import dotenv from 'dotenv'
+
+dotenv.config({ path: './env' })
+const allowedOrigins = [
     'http://localhost:3000',
     'http://localhost:8080',
    'http://192.168.3.117:3000',
-   'http://192.168.3.117:8080'
+   'http://192.168.3.117:8080',
+   'http://192.168.2.41:3000',
+   'http://192.168.2.41:8080',
+  'http://192.168.13.48:3000',
+   'http://192.168.13.48:8080',
+  process.env.URL_ORIGIN
 ]
 
 export default allowedOrigins
